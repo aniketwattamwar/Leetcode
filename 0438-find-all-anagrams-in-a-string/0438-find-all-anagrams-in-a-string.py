23d2bcd43854637a2ef52f7dafe9b9594f4d6051
Aniket  Wattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2025-08-19 21:40:37 -0600

    Time: 8477 ms (5%), Space: 18.6 MB (15.07%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -4,12 +4,12 @@ class Solution:
         
         
         freq_p = Counter(p)
-        print(freq_p)
+        
         res = []
         for i in range(len(s)):
             substr = s[i:i+len(p)]
             freq_substr = Counter(substr)
-            print(freq_substr)
+             
             if freq_substr == freq_p:
                 res.append(i)
         return res
