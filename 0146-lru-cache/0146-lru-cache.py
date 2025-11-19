b1a27c472e45a32dbc42eeff095779d43a3edde0
Aniket  Wattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2025-11-18 19:54:00 -0700

    Time: 136 ms (58.48%), Space: 78 MB (43.46%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,26 +1,31 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__(self,key, val):
         self.prev = None
         self.next = None
-        self.val = val
         self.key = key
+        self.val = val
 
 class LRUCache:
-
+#  
+    # left      (4,4)  (3,3)        right
+    # least                           most
+    # {1, Node(1,1)}
+    
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.hashmap = {}
-        self.left = Node(0, 0)
-        self.right = Node(0, 0)
+        self.left = Node(0,0)
+        self.right = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
 
-
+    
     def _remove(self, node):
+        
         node.prev.next = node.next
-        node.next.prev  = node.prev
+        node.next.prev = node.prev
 
-
+    
     def _add(self, node):
         
         self.right.prev.next = node
@@ -29,17 +34,19 @@ class LRUCache:
         node.next = self.right
         self.right.prev = node
 
+
     def get(self, key: int) -> int:
         if key in self.hashmap:
             node = self.hashmap[key]
             self._remove(node)
             self._add(node)
             return node.val
-        
+
         return -1
 
+
     def put(self, key: int, value: int) -> None:
-        
+
         if key in self.hashmap:
             node = self.hashmap[key]
             node.val = value
@@ -47,17 +54,14 @@ class LRUCache:
             self._add(node)
             return
         
-        
         if len(self.hashmap) == self.capacity:
             del self.hashmap[self.left.next.key]
             self._remove(self.left.next)
-            
-        node = Node(key, value)
+
+        node = Node(key,value)
         self.hashmap[key] = node
         self._add(node)
 
-
-    
         
 
 
@@ -65,4 +69,3 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
-
