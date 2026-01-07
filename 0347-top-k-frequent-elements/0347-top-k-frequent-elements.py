86ea0171c274d8de9274a221a46af44c5fbdd8b8
Aniket  Wattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2026-01-07 14:51:36 -0700

    Time: 0 ms (100%), Space: 23.2 MB (18.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,17 +3,35 @@ import collections
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        
         freq = Counter(nums)
-        print(freq)
-        max_heap = []
-        for num, f in freq.items():
-            heapq.heappush(max_heap, (-f, num))
-        k_freq = []
-        for i in range(k):
-            f, num = heapq.heappop(max_heap)
-            k_freq.append(num)
+        min_heap = []
 
-        return k_freq
+        for num, f in freq.items():
+            heapq.heappush(min_heap, (f,num))
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+            
+
+        print(min_heap)
+        ans = []
+        for i in range(len(min_heap)):
+            f,n = heapq.heappop(min_heap)
+            ans.append(n)
+        return ans
+
+        # Got below soution on my own, but the max heap is a trap and the solution is NlogN
+        # freq = Counter(nums)
+        # print(freq)
+        # max_heap = []
+        # for num, f in freq.items():
+        #     heapq.heappush(max_heap, (-f, num))
+        # k_freq = []
+        # for i in range(k):
+        #     f, num = heapq.heappop(max_heap)
+        #     k_freq.append(num)
+
+        # return k_freq
 
 
 
