2d967f71a98f7a8150dcaa5b1c72146261880f28
Aniket  Wattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2026-01-08 00:38:59 -0700

    Time: 90 ms (96%), Space: 76.9 MB (99.77%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,66 +1,73 @@
 class Node:
-    def __init__(self,key, val):
-        self.prev = None
-        self.next = None
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
+        self.value = value
+        self.next = None
+        self.prev = None
 
 class LRUCache:
-#  
-    # left      (4,4)  (3,3)        right
-    # least                           most
-    # {1, Node(1,1)}
-    
+
     def __init__(self, capacity: int):
+        self.hm = {}
         self.capacity = capacity
-        self.hashmap = {}
         self.left = Node(0,0)
         self.right = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
 
+    def _insert(self,node):
+        
+        prev_node = self.right.prev
+        nxt_node = self.right
+
+        node.prev = prev_node
+        node.next = nxt_node
+
+        prev_node.next = node
+        nxt_node.prev = node
+
     
     def _remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
         
-        node.prev.next = node.next
-        node.next.prev = node.prev
-
-    
-    def _add(self, node):
-        
-        self.right.prev.next = node
-        node.prev = self.right.prev
-
-        node.next = self.right
-        self.right.prev = node
-
-
     def get(self, key: int) -> int:
-        if key in self.hashmap:
-            node = self.hashmap[key]
-            self._remove(node)
-            self._add(node)
-            return node.val
 
-        return -1
-
+        if key not in self.hm:
+            return -1
+        
+        node = self.hm[key]
+        self._remove(node)
+        self._insert(node)
+        return node.value
+        
+        
 
     def put(self, key: int, value: int) -> None:
-
-        if key in self.hashmap:
-            node = self.hashmap[key]
-            node.val = value
+        
+        if key in self.hm:
+            node = self.hm[key]
+            node.value = value
             self._remove(node)
-            self._add(node)
+            self._insert(node)
             return
         
-        if len(self.hashmap) == self.capacity:
-            del self.hashmap[self.left.next.key]
+        if self.capacity <= len(self.hm):
+            del self.hm[self.left.next.key]
             self._remove(self.left.next)
-
+            
+        
         node = Node(key,value)
-        self.hashmap[key] = node
-        self._add(node)
+        self._insert(node)
+        self.hm[key] = node
+
+        
+
+            
+            
 
         
 
