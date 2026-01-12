050872bf549f5418e07e7365fb6547c856eba011
Aniket  Wattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2026-01-11 22:50:10 -0700

    Time: 177 ms (94.05%), Space: 30.3 MB (27.84%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,24 +1,55 @@
 import heapq
 from sortedcontainers import SortedDict
+from collections import deque
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         n = len(nums)
-        window = SortedDict()
+        # Approach 4. The most efficient with 2 deques
+        max_deque = deque()
+        min_deque = deque()
         l = 0
         max_ans = 0
+
         for r in range(n):
-            if nums[r] in window:
-                window[nums[r]]+=1
-            else:
-                window[nums[r]] = 1
-         
-            while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
-                window[nums[l]] -=1
-                if window[nums[l]] == 0:
-                    window.pop(nums[l])
+            while max_deque and max_deque[-1] < nums[r]:
+                max_deque.pop()
+            max_deque.append(nums[r])
+             
+
+            while min_deque and min_deque[-1] > nums[r]:
+                min_deque.pop()
+            min_deque.append(nums[r])
+            
+
+            while max_deque[0] - min_deque[0] > limit:
+                if max_deque[0] == nums[l]:
+                    max_deque.popleft()
+                if min_deque[0] == nums[l]:
+                    min_deque.popleft()
                 l+=1
             max_ans = max(max_ans, r - l +1)
+                
         return max_ans
+    
+        
+        
+        # Approach 3 with sorted dicti - new concept for me
+        # window = SortedDict()
+        # l = 0
+        # max_ans = 0
+        # for r in range(n):
+        #     if nums[r] in window:
+        #         window[nums[r]]+=1
+        #     else:
+        #         window[nums[r]] = 1
+         
+        #     while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
+        #         window[nums[l]] -=1
+        #         if window[nums[l]] == 0:
+        #             window.pop(nums[l])
+        #         l+=1
+        #     max_ans = max(max_ans, r - l +1)
+        # return max_ans
         
         
         # Approach 2 with 2 heaps
