c75b00a26f11cd21ea526df7a4cf14226db0c9d8
Aniket  Wattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2026-01-11 22:41:49 -0700

    Time: 952 ms (5.04%), Space: 31.3 MB (24.26%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,76 @@
+import heapq
+from sortedcontainers import SortedDict
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        window = SortedDict()
+        l = 0
+        max_ans = 0
+        for r in range(n):
+            if nums[r] in window:
+                window[nums[r]]+=1
+            else:
+                window[nums[r]] = 1
+         
+            while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
+                window[nums[l]] -=1
+                if window[nums[l]] == 0:
+                    window.pop(nums[l])
+                l+=1
+            max_ans = max(max_ans, r - l +1)
+        return max_ans
+        
+        
+        # Approach 2 with 2 heaps
+        # minheap = []
+        # maxheap = []
+        # l = 0
+        # r = 0
+        # max_ans = 0
+        # ans = 0
+        # for r in range(n):
+        #     heapq.heappush(minheap, (nums[r], r))
+        #     heapq.heappush(maxheap, (-nums[r], r))
+            
+        #     diff = abs(-maxheap[0][0] - minheap[0][0])
+        #     if diff > limit:
+        #         if minheap[0][0] == nums[l]:
+        #             heapq.heappop(minheap)
+        #         elif maxheap[0][0] == nums[l]:
+        #             heapq.heappop(maxheap)
+        #         l+=1
+               
+        #         ans = r - l
+             
+
+        #     max_ans = max(max_ans, ans)
+
+        # return max_ans
+            
+
+        # TLE approach with two pointers.
+        # l = 0
+        # r = 0
+        # min_val = 0
+        # max_val = 0
+        # max_ans = 0 
+        # ans = 0
+        # while l <= r and r < n:
+            
+        #     min_val = min(nums[l:r+1])
+        #     max_val = max(nums[l:r+1])
+
+        #     diff = abs(min_val - max_val)
+        #     if diff > limit:
+        #         l += 1
+        #         ans = r - l 
+        #     else:
+        #         r+=1
+        #         ans = r - l
+        #     max_ans = max(max_ans, ans)
+        
+        # return max_ans
+            
+               
+
+
