7636485321955bcaf3db0742bfbae3de883355d5
Aniket  Wattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2026-01-31 11:39:06 -0700

    Time: 4 ms (70.76%), Space: 23.1 MB (27.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,18 +2,36 @@ import heapq
 import collections
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        
+             
+        # d = Counter(nums)
+
+
+        # sorted_d = sorted(d.items(), key=lambda x: x[1], reverse=True)
+        # print(sorted_d)
+        # ans = []
+        # for i in range(k):
+        #     k,f = sorted_d[i]
+        #     print(sorted_d[i])
+        #     ans.append(k)
+        # return ans
+
+        import heapq
         d = Counter(nums)
+        h = []
+        for key,f in d.items():
+            if len(h) >= k:
+                if f > h[0][0]:
+                    heapq.heappop(h)
+                    heapq.heappush(h, (f,key))
+            else:
+                heapq.heappush(h,(f,key))
 
+        topk=[]
+        print(h)
+        for ele in h:
+            topk.append(ele[1])
 
-        sorted_d = sorted(d.items(), key=lambda x: x[1], reverse=True)
-        print(sorted_d)
-        ans = []
-        for i in range(k):
-            k,f = sorted_d[i]
-            print(sorted_d[i])
-            ans.append(k)
-        return ans
+        print(topk)
+        return topk
 
 
